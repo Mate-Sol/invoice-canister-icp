58fc6c75140cccd8cda1a654cdc9dfa53b2fce07
rahman-habib <habib.rahman4665@gmail.com>
2023-11-29 13:46:56 +0500

    commit

--- up.sh
+++ up.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#
-# Copyright IBM Corp All Rights Reserved
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# Exit on first error
+
 
 
  dfx stop
@@ -15,17 +10,3 @@
 pushd ../Invoice-Apis
 nodemon app.js
 
-#node enrollAdmin
-
-#node registerUser
-
-#pm2 restart 3
-#node app.js
-#popd
-
-#pushd ./explorer
-#docker-compose down
-#docker volume rm explorer_pgdata
-#docker volume rm explorer_walletstore
-
-#popd
